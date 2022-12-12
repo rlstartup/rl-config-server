47b03f12843ba50391d01760a03e86c758273306
Lucas Calebe <lucascalebe07@hotmail.com>
2022-12-11 21:28:14 -0300

    :tada: create project

---
+++ src/main/java/com/example/rlconfigserver/RlConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rlconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rlconfigserver/RlConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rlconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
